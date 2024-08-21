2c6e4112147c2b03e34068b924370ca1998477ed
José Carlos Pereira Silva Filho <jcps.filho@gmail.com>
2024-08-20 21:41:07 -0300

    Instalando dependencias e configurando SQLite

--- project-manager-api/src/app.module.ts
+++ project-manager-api/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { ProjectsModule } from './projects/projects.module';
 import { UsersModule } from './users/users.module';
 import { TasksModule } from './tasks/tasks.module';
+import { TypeormModule } from './modules/config/typeorm/typeorm.module';
 
 @Module({
-  imports: [ProjectsModule, UsersModule, TasksModule],
+  imports: [ProjectsModule, UsersModule, TasksModule, TypeormModule],
   controllers: [AppController],
   providers: [AppService],
 })
