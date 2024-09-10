d88e8f79e94135855eb77622e9c68ad62cad7c59
José Carlos Pereira Silva Filho <jcps.filho@gmail.com>
2024-09-10 20:27:14 -0300

    Adicionando Redis como ferramenta de cache

--- project-manager-api/src/app.module.ts
+++ project-manager-api/src/app.module.ts
@@ -6,9 +6,23 @@ import { UsersModule } from './users/users.module';
 import { TasksModule } from './tasks/tasks.module';
 import { TypeOrmConfig } from './modules/config/typeorm/typeorm.module';
 import { PaginationModule } from './modules/pagination/pagination.module';
+import { CacheModule } from '@nestjs/cache-manager';
+import * as redisStore from "cache-manager-redis-store";
 
 @Module({
-  imports: [ProjectsModule, UsersModule, TasksModule, TypeOrmConfig, PaginationModule],
+  imports: [
+    ProjectsModule, 
+    UsersModule, 
+    TasksModule, 
+    TypeOrmConfig, 
+    PaginationModule, 
+    CacheModule.register({
+      isGlobal: true,
+      store: redisStore,
+      host: process.env.REDIS_HOST,
+      port: process.env.REDIS_PORT,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
